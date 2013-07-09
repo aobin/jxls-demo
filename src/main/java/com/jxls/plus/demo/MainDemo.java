0974459874ab25aa5de961a8eda97c18e1137994
Leonid Vysochyn <leonate@gmail.com>
2013-07-09 17:59:54 +0300

    Object collection demo for guide doc

--- src/main/java/com/jxls/plus/demo/MainDemo.java
+++ src/main/java/com/jxls/plus/demo/MainDemo.java
@@ -1,33 +1,36 @@
 package com.jxls.plus.demo;
 
+import com.jxls.plus.demo.guide.ObjectCollectionDemo;
 import jxl.read.biff.BiffException;
 import jxl.write.WriteException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 /**
  * @author Leonid Vysochyn
  *         Date: 2/9/12 5:05 PM
  */
 public class MainDemo {
-    public static void main(String[] args) throws IOException, InvalidFormatException, WriteException, BiffException {
-        EachIfCommandDemo.execute();
-        EachIfXmlBuilderDemo.execute();
-        FormulaExportDemo.execute();
-        AreaListenerDemo.execute();
-        MultipleSheetDemo.execute();
-        UserCommandDemo.execute();
-        XlsCommentBuilderDemo.execute();
-        JexcelXlsCommentBuilderDemo.execute();
-        ImageDemo.execute();
-        StressDemo.executeStress1();
-        StressDemo.executeStress2();
-        StressXlsxDemo.executeStress1();
-        StressXlsxDemo.executeStress2();
-        SxssfDemo.executeStress1();
-        SxssfDemo.executeStress2();
-        JexcelStressDemo.executeStress1();
-        JexcelStressDemo.executeStress2();
+    public static void main(String[] args) throws IOException, InvalidFormatException, WriteException, BiffException, ParseException {
+        ObjectCollectionDemo.main(args);
+//        EachIfCommandDemo.execute();
+//        EachIfXmlBuilderDemo.execute();
+//        FormulaExportDemo.execute();
+//        AreaListenerDemo.execute();
+//        MultipleSheetDemo.execute();
+//        UserCommandDemo.execute();
+//        XlsCommentBuilderDemo.execute();
+//        JexcelXlsCommentBuilderDemo.execute();
+//        ImageDemo.execute();
+//        StressDemo.executeStress1();
+//        StressDemo.executeStress2();
+//        StressXlsxDemo.executeStress1();
+//        StressXlsxDemo.executeStress2();
+//        SxssfDemo.executeStress1();
+//        SxssfDemo.executeStress2();
+//        JexcelStressDemo.executeStress1();
+//        JexcelStressDemo.executeStress2();
     }
 }
