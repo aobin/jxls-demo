45a29f32292945c1234cc5e24edb970152b1dbec
Leonid Vysochyn <leonate@gmail.com>
2015-07-01 01:45:19 +0200

    Grid demo

--- src/main/java/org/jxls/demo/MainDemo.java
+++ src/main/java/org/jxls/demo/MainDemo.java
@@ -1,5 +1,6 @@
 package org.jxls.demo;
 
+import org.jxls.command.GridCommand;
 import org.jxls.demo.guide.*;
 import org.jxls.demo.reader.XlsReaderDemo;
 import org.jxls.util.TransformerFactory;
@@ -26,6 +27,7 @@ public class MainDemo {
         XlsCommentBuilderDemo.main(args);
         ImageDemo.main(args);
         DynamicColumnsDemo.main(args);
+        GridCommandDemo.main(args);
 
         XlsReaderDemo.main(args);
 
