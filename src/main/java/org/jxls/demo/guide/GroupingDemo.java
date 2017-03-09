8944a2c02684912ba1beaeb505b0ef8745783948
Leonid Vysochyn <leonate@gmail.com>
2017-03-09 23:11:31 +0100

    issue#70 - added support for grouping in EachCommand

--- src/main/java/org/jxls/demo/guide/GroupingDemo.java
+++ src/main/java/org/jxls/demo/guide/GroupingDemo.java
@@ -15,18 +15,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-/**
- * Object collection output demo
- * @author Leonid Vysochyn
- */
-public class ObjectCollectionDemo {
-    private static Logger logger = LoggerFactory.getLogger(ObjectCollectionDemo.class);
+public class GroupingDemo {
+    private static Logger logger = LoggerFactory.getLogger(GroupingDemo.class);
 
     public static void main(String[] args) throws ParseException, IOException {
-        logger.info("Running Object Collection demo");
+        logger.info("Running Grouping demo");
         List<Employee> employees = generateSampleEmployeeData();
-        try(InputStream is = ObjectCollectionDemo.class.getResourceAsStream("object_collection_template.xls")) {
-            try (OutputStream os = new FileOutputStream("target/object_collection_output.xls")) {
+        try(InputStream is = GroupingDemo.class.getResourceAsStream("grouping_template.xlsx")) {
+            try (OutputStream os = new FileOutputStream("target/grouping_output.xlsx")) {
                 Context context = new Context();
                 context.putVar("employees", employees);
                 JxlsHelper.getInstance().processTemplate(is, os, context);
@@ -39,9 +35,13 @@ public class ObjectCollectionDemo {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MMM-dd", Locale.US);
         employees.add( new Employee("Elsa", dateFormat.parse("1970-Jul-10"), 1500, 0.15) );
         employees.add( new Employee("Oleg", dateFormat.parse("1973-Apr-30"), 2300, 0.25) );
+        employees.add( new Employee("John", dateFormat.parse("1970-Jul-10"), 3500, 0.10) );
         employees.add( new Employee("Neil", dateFormat.parse("1975-Oct-05"), 2500, 0.00) );
         employees.add( new Employee("Maria", dateFormat.parse("1978-Jan-07"), 1700, 0.15) );
         employees.add( new Employee("John", dateFormat.parse("1969-May-30"), 2800, 0.20) );
+        employees.add( new Employee("Oleg", dateFormat.parse("1988-Apr-30"), 1500, 0.15) );
+        employees.add( new Employee("Maria", dateFormat.parse("1970-Jul-10"), 3000, 0.10) );
+        employees.add( new Employee("John", dateFormat.parse("1973-Apr-30"), 1000, 0.05) );
         return employees;
     }
 }
