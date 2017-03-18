4578c93737499c32c246b377465484f75fd27c52
Leonid Vysochyn <leonate@gmail.com>
2017-03-18 15:38:28 +0100

    complete SXSSF formulas demo

--- src/main/java/org/jxls/demo/SxssfDemo.java
+++ src/main/java/org/jxls/demo/SxssfDemo.java
@@ -29,27 +29,52 @@ import java.util.List;
  * @author Leonid Vysochyn
  */
 public class SxssfDemo {
-    static class TotalCellUpdater implements CellDataUpdater{
-        private static final String SOURCE_FORMULA = "C4*(1+D4)";
+    public static final int EMPLOYEE_COUNT = 30000;
+    public static final int DEPARTMENT_COUNT = 100;
+    public static final int DEP_EMPLOYEE_COUNT = 500;
+    public static Logger logger = LoggerFactory.getLogger(SxssfDemo.class);
+
+    static class CellRefUpdater implements CellDataUpdater{
         @Override
         public void updateCellData(CellData cellData, CellRef targetCell, Context context) {
-            if( cellData.isFormulaCell() && cellData.getFormula().equals(SOURCE_FORMULA) ){
-                cellData.setEvaluationResult(SOURCE_FORMULA.replaceAll("4", Integer.toString(targetCell.getRow()+1)));
+            if( cellData.isFormulaCell() && cellData.getFormula() != null ){
+                cellData.setEvaluationResult(cellData.getFormula().replaceAll("(?<=[A-Za-z])\\d", Integer.toString(targetCell.getRow()+1)));
+            }
+        }
+    }
+    static class TotalCellUpdater implements CellDataUpdater{
+
+        @Override
+        public void updateCellData(CellData cellData, CellRef targetCell, Context context) {
+            if( cellData.isFormulaCell() && cellData.getFormula().equals("SUM(E2)")){
+                String resultFormula = String.format("SUM(E2:E%d)", targetCell.getRow());
+                cellData.setEvaluationResult(resultFormula);
             }
         }
     }
 
-    public static final int EMPLOYEE_COUNT = 30000;
-    public static final int DEPARTMENT_COUNT = 100;
-    public static final int DEP_EMPLOYEE_COUNT = 500;
-    static Logger logger = LoggerFactory.getLogger(SxssfDemo.class);
+    static class DepartmentTotalCellUpdater implements CellDataUpdater{
+        @Override
+        public void updateCellData(CellData cellData, CellRef targetCell, Context context) {
+            if( cellData.isFormulaCell() && cellData.getFormula().equals("SUM(E5)")){
+                Department department = (Department) context.getVar("department");
+                if( department != null ) {
+                    String resultFormula = String.format("SUM(E%d:E%d)",
+                            targetCell.getRow() - department.getStaff().size() + 1, targetCell.getRow());
+                    cellData.setEvaluationResult(resultFormula);
+                }else{
+                    logger.warn("Cannot update the total cell since department is null");
+                }
+            }
+        }
+    }
+
 
     public static void main(String[] args) throws IOException, InvalidFormatException, ParseException {
         logger.info("Entering Stress Sxssf demo");
-//        demoDisableFormulaCellRefProcessing();
         simpleSxssf();
-//        executeStress1();
-//        executeStress2();
+        executeStress1();
+        executeStress2();
     }
 
     public static void simpleSxssf() throws ParseException, IOException, InvalidFormatException {
@@ -63,12 +88,12 @@ public class SxssfDemo {
                 List<Area> xlsAreaList = areaBuilder.build();
                 Area xlsArea = xlsAreaList.get(0);
                 Context context = new Context();
-                context.putVar("totalCellUpdater", new TotalCellUpdater());
+                context.putVar("cellRefUpdater", new CellRefUpdater());
                 context.putVar("employees", employees);
                 xlsArea.applyAt(new CellRef("Result!A1"), context);
-                context.getConfig().setIsFormulaProcessingRequired(false);
-//                xlsArea.processFormulas();
+                context.getConfig().setIsFormulaProcessingRequired(false); // with SXSSF you cannot use normal formula processing
                 workbook.setForceFormulaRecalculation(true);
+                workbook.setActiveSheet(1);
                 ((PoiTransformer) transformer).getWorkbook().write(os);
             }
         }
@@ -79,7 +104,7 @@ public class SxssfDemo {
         logger.info("Generating " + EMPLOYEE_COUNT + " employees..");
         List<Employee> employees = Employee.generate(EMPLOYEE_COUNT);
         logger.info("Created " + employees.size() + " employees");
-        try(InputStream is = SxssfDemo.class.getResourceAsStream("stress1.xlsx")) {
+        try(InputStream is = SxssfDemo.class.getResourceAsStream("stress1_sxssf.xlsx")) {
             assert is != null;
             Workbook workbook = WorkbookFactory.create(is);
             Transformer transformer = PoiTransformer.createSxssfTransformer(workbook);
@@ -87,12 +112,16 @@ public class SxssfDemo {
             List<Area> xlsAreaList = areaBuilder.build();
             Area xlsArea = xlsAreaList.get(0);
             Context context = new PoiContext();
+            context.putVar("cellRefUpdater", new CellRefUpdater());
+            context.putVar("totalCellUpdater", new TotalCellUpdater());
             context.putVar("employees", employees);
+            context.getConfig().setIsFormulaProcessingRequired(false);
             long startTime = System.nanoTime();
-            xlsArea.applyAt(new CellRef("NewSheet!A1"), context);
-//        xlsArea.processFormulas();
+            xlsArea.applyAt(new CellRef("Result!A1"), context);
             long endTime = System.nanoTime();
             System.out.println("Stress Sxssf demo 1 time (s): " + (endTime - startTime) / 1000000000);
+            workbook.setForceFormulaRecalculation(true);
+            workbook.setActiveSheet(1);
             try(OutputStream os = new FileOutputStream("target/sxssf_stress1_output.xlsx")) {
                 ((PoiTransformer) transformer).getWorkbook().write(os);
             }
@@ -117,7 +146,7 @@ public class SxssfDemo {
             long startTime = System.nanoTime();
             xlsArea.applyAt(new CellRef("NewSheet!A1"), context);
             long endTime = System.nanoTime();
-            System.out.println("Stress Sxssf demo 1 time (s): " + (endTime - startTime) / 1000000000);
+            System.out.println("Stress Sxssf demo 1 time (s): " + (endTime - startTime) / 1e9);
             try(OutputStream os = new FileOutputStream("target/sxssf_stress1_output.xlsx")) {
                 ((PoiTransformer) transformer).getWorkbook().write(os);
             }
@@ -129,7 +158,7 @@ public class SxssfDemo {
         logger.info("Generating " + DEPARTMENT_COUNT + " departments with " + DEP_EMPLOYEE_COUNT + " employees in each");
         List<Department> departments = Department.generate(DEPARTMENT_COUNT, DEP_EMPLOYEE_COUNT);
         logger.info("Created " + departments.size() + " departments");
-        try(InputStream is = SxssfDemo.class.getResourceAsStream("stress2.xlsx")) {
+        try(InputStream is = SxssfDemo.class.getResourceAsStream("stress2_sxssf.xlsx")) {
             Workbook workbook = WorkbookFactory.create(is);
             // setting rowAccessWindowSize to 600 to be able to process static cells in a single iteration
             Transformer transformer = PoiTransformer.createSxssfTransformer(workbook, 600, true);
@@ -137,12 +166,16 @@ public class SxssfDemo {
             List<Area> xlsAreaList = areaBuilder.build();
             Area xlsArea = xlsAreaList.get(0);
             Context context = new PoiContext();
+            context.getConfig().setIsFormulaProcessingRequired(false);
+            context.putVar("cellRefUpdater", new CellRefUpdater());
+            context.putVar("totalCellUpdater", new DepartmentTotalCellUpdater());
             context.putVar("departments", departments);
             long startTime = System.nanoTime();
-            xlsArea.applyAt(new CellRef("NewSheet!A1"), context);
-//        xlsArea.processFormulas();
+            xlsArea.applyAt(new CellRef("Result!A1"), context);
             long endTime = System.nanoTime();
-            System.out.println("Stress Sxssf demo 2 time (s): " + (endTime - startTime) / 1000000000);
+            System.out.println("Stress Sxssf demo 2 time (s): " + (endTime - startTime) / 1e9);
+            workbook.setForceFormulaRecalculation(true);
+            workbook.setActiveSheet(1);
             try (OutputStream os = new FileOutputStream("target/sxssf_stress2_output.xlsx")) {
                 ((PoiTransformer) transformer).getWorkbook().write(os);
             }
